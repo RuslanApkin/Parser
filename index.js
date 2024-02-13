11bac5a1acb6b4d02786e5c8a47afc97429079f3
RuslanApkin <ruslan-apkin@yandex.ru>
2024-02-13 21:16:51 +0500

    node script is done

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 const puppeteer = require("puppeteer");
+const { regionList, selectorList } = require("./config.json");
+var fs = require("fs");
 
 (async () => {
     const url = process.argv[2];
     const region = process.argv[3];
+    const id = process.argv[4];
+    let savePath = "./results/" + region;
+    fs.mkdir(savePath, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+    savePath += "/" + id + "_";
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
     page.setViewport({
-        width: 1920,
-        height: 1080,
+        width: 1280,
+        height: 740,
         deviceScaleFactor: 1
     });
+
+    // Выбор региона
+    const { regionId } = regionList.find(({ name }) => name === region);
     await page.setCookie({
         name: "region",
-        value: "8",
+        value: regionId.toString(),
         domain: "www.vprok.ru"
     });
+
     await page.goto(url, { waitUntil: "networkidle2" });
 
     // Скриншот страницы
     await page.screenshot({
-        path: "screenshot.jpg",
+        path: savePath + "screenshot.jpg",
         type: "jpeg",
         fullPage: "true"
     });
+
+    // Сбор информации о товаре
+    let output = "";
+    for (const selector of selectorList) {
+        const element = await page.$(selector.value);
+        if (element !== null) {
+            const value = await page.evaluate((el) => el.innerText, element);
+            output += selector.name + "=" + value.split(" ")[0] + "\n";
+        }
+    }
+
+    // Запись в файл
+    await fs.promises.writeFile(savePath + "product.txt", output);
 
     await browser.close();
 })();
